6ae8b351760795712c02989ea9eb0827249b32ac
Kyle Steinle <kyledavid022@gmail.com>
2017-06-18 00:36:09 -0700

    added comments. added item sorting in mix container

--- js/mixer.js
+++ js/mixer.js
@@ -1,12 +1,36 @@
 $(document).ready(function() {
 
 	var controlButtons = document.querySelector('.mix-controls').querySelectorAll('button');
-
+	
 	controlButtons.forEach(function(button) {
 		button.addEventListener('mouseup', filterItems);
 	});
 
+	sortContainerItems();
+
+	/* Sort all items in a .mix-container alphabetically by taxonomy data */
+
+	function sortContainerItems() {
+		var mixItems = document.querySelectorAll('.mix');
+		var mixArray = [...mixItems];
+
+
+		mixArray = (mixArray).sort(function(firstBox, secondBox) {
+			var firstTax = firstBox.dataset.tax;
+			var secondTax = secondBox.dataset.tax;
+
+			return firstTax.charAt(0) > secondTax.charAt(0);
+		});
+
+		mixArray.forEach(function(item) {
+			document.querySelector('.mix-container').appendChild(item);
+		});
+	}
+
+	/* determine how to filter mix items */
+
 	function filterItems() {
+
 		var filter = this.dataset.filter;
 
 		if (filter === 'all') {
@@ -19,15 +43,10 @@ $(document).ready(function() {
 
 	}
 
-	function showAllItems() {
-		var mixItems = document.querySelectorAll('.mix');
-
-		$(mixItems).each(function(i, item) {
-			$(item).show();
-		});
-	}
+	/* Hide all items with the target filter data */
 
 	function applyFilter(filter) {
+
 		var mixItems = document.querySelectorAll('.mix');
 		var filteredItems = $(mixItems).filter(filter);
 
@@ -37,12 +56,21 @@ $(document).ready(function() {
 		});
 
 		$(filteredItems).each(function(i, item) {
-			$(item).hide();
+			$(item).fadeOut();
 		});
 
 		$(otherItems).each(function(i, item) {
-			$(item).show();
+			$(item).fadeIn();
 		})
+	}
+
+	function showAllItems() {
+		
+		var mixItems = document.querySelectorAll('.mix');
+
+		$(mixItems).each(function(i, item) {
+			$(item).fadeIn();
+		});
 	}
 	
 });
