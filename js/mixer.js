70a8e278fe540d8f86584bb11670128538efc029
Kyle Steinle <kyledavid022@gmail.com>
2017-06-17 18:26:11 -0700

    added button controls

--- js/mixer.js
+++ js/mixer.js
@@ -1,10 +1,48 @@
 $(document).ready(function() {
 
-	var mixItems = document.querySelectorAll('.mix');
-	var newList = $(mixItems).filter('.green-box');
+	var controlButtons = document.querySelector('.mix-controls').querySelectorAll('button');
 
-	$(newList).each(function(i, item) {
-		$(item).hide();
+	controlButtons.forEach(function(button) {
+		button.addEventListener('mouseup', filterItems);
 	});
 
+	function filterItems() {
+		var filter = this.dataset.filter;
+
+		if (filter === 'all') {
+			showAllItems();
+		} else if (filter === 'none') {
+			applyFilter('.mix');
+		} else {
+			applyFilter(filter);
+		}	
+
+	}
+
+	function showAllItems() {
+		var mixItems = document.querySelectorAll('.mix');
+
+		$(mixItems).each(function(i, item) {
+			$(item).show();
+		});
+	}
+
+	function applyFilter(filter) {
+		var mixItems = document.querySelectorAll('.mix');
+		var filteredItems = $(mixItems).filter(filter);
+
+		var otherItems = $(mixItems).filter(function(i, item) {
+			console.log(item.classList.contains(filter.substr(1)));
+			return !item.classList.contains(filter.substr(1));
+		});
+
+		$(filteredItems).each(function(i, item) {
+			$(item).hide();
+		});
+
+		$(otherItems).each(function(i, item) {
+			$(item).show();
+		})
+	}
+	
 });
